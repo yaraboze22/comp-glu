8be82e8ef9b6a52eddda3b78e40d5c2ee5e552d6
yaraboze22 <105108137+yaraboze22@users.noreply.github.com>
2022-06-24 22:15:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include<math.h>
+#include <stdlib.h>
 
+ 
+
+GLfloat cloud1x = 0.0f;  
+GLfloat cloud2x = 0.0f; 
+GLfloat angle = 0.0f;   
+GLfloat carAndWheelX = 0;  
+GLint red;   
+ 
+ 
+GLfloat redf = 1.0f; 
