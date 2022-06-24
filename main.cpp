5a629598e8e10ec76c126e423dbed060cd4dccfb
yaraboze22 <105108137+yaraboze22@users.noreply.github.com>
2022-06-24 22:16:45 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,17 @@
 #endif
 #include<math.h>
 #include <stdlib.h>
-
- 
-
 GLfloat cloud1x = 0.0f;  
 GLfloat cloud2x = 0.0f; 
 GLfloat angle = 0.0f;   
 GLfloat carAndWheelX = 0;  
 GLint red;   
- 
- 
 GLfloat redf = 1.0f; 
+void init() {
+
+    glClearColor(0.31, 0.58, 0.796, 0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluOrtho2D(-100, 100, -100, 100);
+
+}
